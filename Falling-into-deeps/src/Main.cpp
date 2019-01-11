4d9cbadc955cf4209b0bd0b4f9a91ee93e46d568
RoyalKing2004 <44666045+RoyalKing2004@users.noreply.github.com>
2019-01-11 22:25:57 +0300

    Project created.
    Project was created by premake.

---
+++ Falling-into-deeps/src/Main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+int main()
+{
+	std::cout << "Project compiled succesfuly." << std::endl;
+	std::cin.get();
+}
---
+++ premake5.lua
@@ -0,0 +1,51 @@
+workspace "Falling-into-deeps"
+	architecture "x64"
+
+	configurations
+	{
+		"Debug",
+		"Release",
+		"Dist"
+	}
+
+outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+
+project "Falling-into-deeps"
+	location "Falling-into-deeps"
+	kind "ConsoleApp"
+	language "C++"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp"
+	}
+
+	includedirs
+	{
+	}
+
+	filter "system:windows"
+		cppdialect "C++17"
+		staticruntime "On"
+		systemversion "latest"
+
+		defines
+		{
+			"SGT_PLATFORM_WINDOWS"
+		}
+
+	filter "configurations:Debug"
+		defines "SGT_DEBUG"
+		symbols "On"
+
+	filter "configurations:Release"
+		defines "SGT_RELEASE"
+		optimize "On"
+
+	filter "configurations:Dist"
+		defines "SGT_DIST"
+		optimize "On"
