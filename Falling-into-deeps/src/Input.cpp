834b2d7e0b55e49a7b1a10ce82241d5b927dd27f
RustyGuard <44666045+RustyGuard@users.noreply.github.com>
2019-02-09 11:21:40 +0400

    Functional keys added

--- Falling-into-deeps/src/Input.cpp
+++ Falling-into-deeps/src/Input.cpp
@@ -62,76 +62,76 @@ void Gear::InitKeys()
 	SetKey("world_2", GLFW_KEY_WORLD_2);
 
 	/* Function keys */
-	/*SetKey("", GLFW_KEY_ESCAPE             256
-	SetKey("", GLFW_KEY_ENTER              257
-	SetKey("", GLFW_KEY_TAB                258
-	SetKey("", GLFW_KEY_BACKSPACE          259
-	SetKey("", GLFW_KEY_INSERT             260
-	SetKey("", GLFW_KEY_DELETE             261
-	SetKey("", GLFW_KEY_RIGHT              262
-	SetKey("", GLFW_KEY_LEFT               263
-	SetKey("", GLFW_KEY_DOWN               264
-	SetKey("", GLFW_KEY_UP                 265
-	SetKey("", GLFW_KEY_PAGE_UP            266
-	SetKey("", GLFW_KEY_PAGE_DOWN          267
-	SetKey("", GLFW_KEY_HOME               268
-	SetKey("", GLFW_KEY_END                269
-	SetKey("", GLFW_KEY_CAPS_LOCK          280
-	SetKey("", GLFW_KEY_SCROLL_LOCK        281
-	SetKey("", GLFW_KEY_NUM_LOCK           282
-	SetKey("", GLFW_KEY_PRINT_SCREEN       283
-	SetKey("", GLFW_KEY_PAUSE              284
-	SetKey("", GLFW_KEY_F1                 290
-	SetKey("", GLFW_KEY_F2                 291
-	SetKey("", GLFW_KEY_F3                 292
-	SetKey("", GLFW_KEY_F4                 293
-	SetKey("", GLFW_KEY_F5                 294
-	SetKey("", GLFW_KEY_F6                 295
-	SetKey("", GLFW_KEY_F7                 296
-	SetKey("", GLFW_KEY_F8                 297
-	SetKey("", GLFW_KEY_F9                 298
-	SetKey("", GLFW_KEY_F10                299
-	SetKey("", GLFW_KEY_F11                300
-	SetKey("", GLFW_KEY_F12                301
-	SetKey("", GLFW_KEY_F13                302
-	SetKey("", GLFW_KEY_F14                303
-	SetKey("", GLFW_KEY_F15                304
-	SetKey("", GLFW_KEY_F16                305
-	SetKey("", GLFW_KEY_F17                306
-	SetKey("", GLFW_KEY_F18                307
-	SetKey("", GLFW_KEY_F19                308
-	SetKey("", GLFW_KEY_F20                309
-	SetKey("", GLFW_KEY_F21                310
-	SetKey("", GLFW_KEY_F22                311
-	SetKey("", GLFW_KEY_F23                312
-	SetKey("", GLFW_KEY_F24                313
-	SetKey("", GLFW_KEY_F25                314
-	SetKey("", GLFW_KEY_KP_0               320
-	SetKey("", GLFW_KEY_KP_1               321
-	SetKey("", GLFW_KEY_KP_2               322
-	SetKey("", GLFW_KEY_KP_3               323
-	SetKey("", GLFW_KEY_KP_4               324
-	SetKey("", GLFW_KEY_KP_5               325
-	SetKey("", GLFW_KEY_KP_6               326
-	SetKey("", GLFW_KEY_KP_7               327
-	SetKey("", GLFW_KEY_KP_8               328
-	SetKey("", GLFW_KEY_KP_9               329
-	SetKey("", GLFW_KEY_KP_DECIMAL         330
-	SetKey("", GLFW_KEY_KP_DIVIDE          331
-	SetKey("", GLFW_KEY_KP_MULTIPLY        332
-	SetKey("", GLFW_KEY_KP_SUBTRACT        333
-	SetKey("", GLFW_KEY_KP_ADD             334
-	SetKey("", GLFW_KEY_KP_ENTER           335
-	SetKey("", GLFW_KEY_KP_EQUAL           336
-	SetKey("", GLFW_KEY_LEFT_SHIFT         340
-	SetKey("", GLFW_KEY_LEFT_CONTROL       341
-	SetKey("", GLFW_KEY_LEFT_ALT           342
-	SetKey("", GLFW_KEY_LEFT_SUPER         343
-	SetKey("", GLFW_KEY_RIGHT_SHIFT        344
-	SetKey("", GLFW_KEY_RIGHT_CONTROL      345
-	SetKey("", GLFW_KEY_RIGHT_ALT          346
-	SetKey("", GLFW_KEY_RIGHT_SUPER        347
-	SetKey("", GLFW_KEY_MENU               348*/
+	SetKey("escape", GLFW_KEY_ESCAPE);
+	SetKey("enter", GLFW_KEY_ENTER);
+	SetKey("tab", GLFW_KEY_TAB);
+	SetKey("backspace", GLFW_KEY_BACKSPACE);
+	SetKey("insert", GLFW_KEY_INSERT);
+	SetKey("delete", GLFW_KEY_DELETE);
+	SetKey("right", GLFW_KEY_RIGHT);
+	SetKey("left", GLFW_KEY_LEFT);
+	SetKey("down", GLFW_KEY_DOWN);
+	SetKey("up", GLFW_KEY_UP);
+	SetKey("page_up", GLFW_KEY_PAGE_UP);
+	SetKey("page_down", GLFW_KEY_PAGE_DOWN);
+	SetKey("home", GLFW_KEY_HOME);
+	SetKey("end", GLFW_KEY_END);
+	SetKey("caps_lock", GLFW_KEY_CAPS_LOCK);
+	SetKey("scroll_lock", GLFW_KEY_SCROLL_LOCK);
+	SetKey("num_lock", GLFW_KEY_NUM_LOCK);
+	SetKey("print_screen", GLFW_KEY_PRINT_SCREEN);
+	SetKey("pause", GLFW_KEY_PAUSE);
+	SetKey("f1", GLFW_KEY_F1);
+	SetKey("f2", GLFW_KEY_F2);
+SetKey("f3", GLFW_KEY_F3);
+	SetKey("f4", GLFW_KEY_F4);
+	SetKey("f5", GLFW_KEY_F5);
+	SetKey("f6", GLFW_KEY_F6);
+	SetKey("f7", GLFW_KEY_F7);
+	SetKey("f8", GLFW_KEY_F8);
+	SetKey("f9", GLFW_KEY_F9);
+	SetKey("f10", GLFW_KEY_F10);
+	SetKey("f11", GLFW_KEY_F11);
+	SetKey("f12", GLFW_KEY_F12);
+	SetKey("f13", GLFW_KEY_F13);
+	SetKey("f14", GLFW_KEY_F14);
+	SetKey("f15", GLFW_KEY_F15);
+	SetKey("f16", GLFW_KEY_F16);
+	SetKey("f17", GLFW_KEY_F17);
+	SetKey("f18", GLFW_KEY_F18);
+	SetKey("f19", GLFW_KEY_F19);
+	SetKey("f20", GLFW_KEY_F20);
+	SetKey("f21", GLFW_KEY_F21);
+	SetKey("f22", GLFW_KEY_F22);
+	SetKey("f23", GLFW_KEY_F23);
+	SetKey("f24", GLFW_KEY_F24);
+	SetKey("f25", GLFW_KEY_F25);
+	SetKey("kp0", GLFW_KEY_KP_0);
+	SetKey("kp1", GLFW_KEY_KP_1);
+	SetKey("kp2", GLFW_KEY_KP_2);
+	SetKey("kp3", GLFW_KEY_KP_3);
+	SetKey("kp4", GLFW_KEY_KP_4);
+	SetKey("kp5", GLFW_KEY_KP_5);
+	SetKey("kp6", GLFW_KEY_KP_6);
+	SetKey("kp7", GLFW_KEY_KP_7);
+	SetKey("kp8", GLFW_KEY_KP_8);
+	SetKey("kp9", GLFW_KEY_KP_9);
+	SetKey("kp_decimal", GLFW_KEY_KP_DECIMAL);
+	SetKey("kp_divide", GLFW_KEY_KP_DIVIDE);
+	SetKey("kp_multiply", GLFW_KEY_KP_MULTIPLY);
+	SetKey("kp_substract", GLFW_KEY_KP_SUBTRACT);
+	SetKey("kp_add", GLFW_KEY_KP_ADD);
+	SetKey("kp_enter", GLFW_KEY_KP_ENTER);
+	SetKey("kp_equal", GLFW_KEY_KP_EQUAL);
+	SetKey("left_shift", GLFW_KEY_LEFT_SHIFT);
+	SetKey("left_control", GLFW_KEY_LEFT_CONTROL);
+	SetKey("left_alt", GLFW_KEY_LEFT_ALT);
+	SetKey("left_super", GLFW_KEY_LEFT_SUPER);
+	SetKey("right_shift", GLFW_KEY_RIGHT_SHIFT);
+	SetKey("right_control", GLFW_KEY_RIGHT_CONTROL);
+	SetKey("right_alt", GLFW_KEY_RIGHT_ALT);
+	SetKey("right_super", GLFW_KEY_RIGHT_SUPER);
+	SetKey("menu", GLFW_KEY_MENU);
 
 }
 
